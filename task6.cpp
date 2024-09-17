536642c3a0d83c63497ebda1124e082064971d67
Darshan G K <darshangurushanthappa@gmail.com>
2024-09-16 23:30:55 -0500

    task6.cpp
    Displays numbers in ascending and descending order

---
+++ task6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio> 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./task6 N" << std::endl;
+        return 1;
+    }
+    int N = std::stoi(argv[1]);
+    for (int i = 0; i <= N; ++i) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = N; i >= 0; --i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
